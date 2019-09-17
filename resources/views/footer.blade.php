7766360edb4b151ccb143e9be65c9a70f778c091
sulih <sulih055@gmail.com>
2019-09-17 16:16:07 +0700

    first

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,13 @@
+<!-- Footer -->
+<footer class="py-5 bg-info">
+    <div class="container">
+        <p class="m-0 text-center text-white">Copyright 2019</p>
+    </div>
+</footer>
+<!-- END : Footer -->
+
+<!-- Bootstrap core JavaScript -->
+<script src="{{ asset('belajar_laravel/vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('belajar_laravel/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+</body>
+</html>
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,76 @@
+@extends('template')
+@section('title', 'Belajar Koding')
+
+@section('intro-header')
+    <!-- Header -->
+    <header class="intro-header text-white" style="background: url('{{ asset
+
+('belajar_laravel/images/home-bg.jpg') }}') no-repeat bottom center;">
+        <div class="container text-center">
+            <h1>Esupply</h1>
+            <p class="lead">-</p>
+        </div>
+    </header>
+    <!-- END : Header -->
+@endsection
+
+@section('main')
+    <!-- Main -->
+    <section class="main">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 mx-auto">
+                    <h2>Halaman</h2>
+                    <p class="lead">Dengan berkembang pesatnya pasar industri dan pembangunan di indonesia,
+                        Mochamad Fauzan selaku penggerak bisnis melihat peluang untuk membantu mensetabilkan pemasokan barang pasar industri yang saat ini sangat berkembang pesat.
+                        Dengan hal tersebut kami selaku penggerak usaha dari Equili Supply ingin memberikan kontribusi yang dapat membantu keseimbangan antara produsen distributor dan konsumen.
+                        Esupply adalah perushaan yang bergerak di bidang pengadaan barang yang mengutamakan kreatifitas, berkembang, dan berintegritas, dalam memberikan pelayanan terhadap konsumen.
+                        dan kami juga memberikan servis kepada konsumen untuk memberikan solusi yang inovatif dalam pemilihan barang untuk menyesuaikan harga tetap, dengan kualitas yang di inginkan
+                        sesuai dengan kebutuhan konsumen.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="main">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 mx-auto">
+                    <h2>Visi Esuplly ​:</h2>
+                    <p class="lead">Esuplly merupakan perusahan yang bergerak di bidang penyediaan peralatan industri yang inovatif dan terpercaya
+                        dan terpercaya untuk menyediakan peralatan yang sesuai dengan kebutuhan perusahan di indonesia.
+                        Sebagai pemasok barang elektrikal dan mekanikal yang profesional maka dari itu kami seimbangkan dengan bisnis administrasi dan
+                        teknisi yang terkait dengan penilaian materi yang berkelanjutan.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="main">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 mx-auto">
+                    <h2>Misi Esuplly ​:</h2>
+                    <p class="lead">1. Sebagai mitra yang dapat di andalkan Esupply membantu perusahaan untuk menyediakan peralatan industri ( electrical, mechanical, pneumatic, maintenace dan service ) sesuai dengan kebutuhan.
+                        2. Sebagai mitra yang dapat di percaya Esupply berkomitmen untuk menyediakan barang tepat waktu.
+                        3. Sebagai mitra yang solutif, Esuplly menyediakan jasa konsultasi untuk penyediaan barang industri.
+                        4. Sebagai mitra untuk berkembang bersama, Esupply berusahaa mendaga keseimbangan rantai pasok antara produsen, distributor dan konsumen.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="main">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 mx-auto">
+                    <h2>Komitmen Esuplly ​:</h2>
+                    <p class="lead">Kami tahu kunci keberhasilan yang berkelanjutan tidak hanya membayangkan dan merencanakan masa depan,
+                        tetapi juga dalam menyelesaikan tantangan hari ini. Dengan ini kami berkomitmenuntuk membantu pelanggan kami untuk
+                        menemukan solusi teknis yang memberikan hasil jangka pendek maupun jangka panjang.</p>
+                    </div>
+                </div>
+           </div>
+    </section>
+    <!-- END : Main -->
+@endsection
